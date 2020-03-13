f15b7b912a0be6b03fba5e0c12db1808351b44e4
kneeraj825 <kneeraj825.com>
2020-03-13 17:22:24 +0530

    Added:Finding the maximum length of string

--- NUnitTestMaximum/UnitTest1.cs
+++ NUnitTestMaximum/UnitTest1.cs
@@ -6,13 +6,22 @@ namespace NUnitTestMaximum
     [TestFixture]
     public class TestMax
     {
+
         [Test]
-        public void Max_ReturnsExpectedValue()
+        public void Max_ReturnIntegerValue()
         {
             TestMaximumInteger md = new TestMaximumInteger();
-            double actualMax = md.isDoubleArray(10.5, 15.2, 14.3);
-            Assert.AreEqual(15.2, actualMax);
+            double actualMaxInteger = md.isIntegerArray(10, 15, 14);
+            Assert.AreEqual(15, actualMaxInteger);
         }
+        [Test]
+        public void Max_Double()
+        {
+            TestMaximumInteger md = new TestMaximumInteger();
+            double actualMaxDouble = md.isDoubleArray(10.5, 15.2, 14.3);
+            Assert.AreEqual(15.2, actualMaxDouble);
+        }
+       
     }
 
     
--- TestMaximumProblem/Program.cs
+++ TestMaximumProblem/Program.cs
@@ -12,6 +12,23 @@ namespace TestMaximumProblem
             double MaxValue = doubleNumber.Max();
             return MaxValue;
         }
+        public int isIntegerArray(int number1, int number2, int number3)
+        {
+            int[] integerNumber = { number1, number2, number3 };
+            //Finding The Maximum integer In Array
+            int MaxValue = integerNumber.Max();
+            return MaxValue;
+        }
+        public int isStringArray(String first, String second, String third)
+        {
+            int str1 = first.Length;
+            int str2 = second.Length;
+            int str3 = third.Length;
+            //Finding The Maximum integer In Array
+            int[] StringArray = { str1, str2, str3 };
+            int MaxValue = StringArray.Max();
+            return MaxValue;
+        }
         public static void Main(String[] args)
         {
             TestMaximumInteger tm = new TestMaximumInteger();
