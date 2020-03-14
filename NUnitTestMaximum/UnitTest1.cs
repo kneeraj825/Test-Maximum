528661ee32f2a954d8cbb353a800cfbb6e3265e2
kneeraj825 <kneeraj825.com>
2020-03-14 09:18:50 +0530

    Refactor:Putting all three methods in one generic methods

--- NUnitTestMaximum/UnitTest1.cs
+++ NUnitTestMaximum/UnitTest1.cs
@@ -11,17 +11,24 @@ namespace NUnitTestMaximum
         public void Max_ReturnIntegerValue()
         {
             TestMaximumInteger md = new TestMaximumInteger();
-            double actualMaxInteger = md.isIntegerArray(10, 15, 14);
+            int actualMaxInteger = md.maxValueArray(10, 15, 14);
             Assert.AreEqual(15, actualMaxInteger);
         }
         [Test]
         public void Max_Double()
         {
             TestMaximumInteger md = new TestMaximumInteger();
-            double actualMaxDouble = md.isDoubleArray(10.5, 15.2, 14.3);
+            double actualMaxDouble = md.maxValueArray(10.5, 15.2, 14.3);
             Assert.AreEqual(15.2, actualMaxDouble);
         }
-       
+        [Test]
+        public void Max_String()
+        {
+            TestMaximumInteger md = new TestMaximumInteger();
+            string actualMaxDouble = md.maxValueArray("the", "three", "four");
+            Assert.AreEqual("three", actualMaxDouble);
+        }
+
     }
 
     
